b53e8c530c049c1ce8587a8717db2b3385263134
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-12-22 11:55:32 +1100

    eentrypoint for terraform shush

---
+++ terraform-shush/entrypoint.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+set -eu -o pipefail
+
+# Setup remote state in artifactory
+terraform remote config \
+	-backend=s3 \
+	-backend-config="bucket=$REMOTE_STATE_BUCKET" \
+	-backend-config="key=$REMOTE_STATE_KEY" \
+	-backend-config="encrypt=true" \
+	-backend-config="region=$TF_VAR_region"
+
+# Try and pull the remote state if there is any
+terraform remote pull
+
+# run the command decrypting KMS variables
+shush --region $TF_VAR_region exec -- $@ 
+
+# make sure the state is pushed
+terraform remote push
