068c941e0b241f87e683d64d1749ba061865613f
pvdvreede <pvdvreede@gmail.com>
2016-12-23 11:17:16 +1100

    Add trap to shush entrypoint for cleanup.

--- terraform-shush/entrypoint.sh
+++ terraform-shush/entrypoint.sh
@@ -2,19 +2,24 @@
 
 set -eu -o pipefail
 
+function cleanup {
+  # make sure the state is pushed
+  terraform remote push
+}
+
+trap cleanup EXIT
+
 # Setup remote state in artifactory
 terraform remote config \
-	-backend=s3 \
-	-backend-config="bucket=$REMOTE_STATE_BUCKET" \
-	-backend-config="key=$REMOTE_STATE_KEY" \
-	-backend-config="encrypt=true" \
-	-backend-config="region=$TF_VAR_region"
+  backend=s3 \
+  backend-config="bucket=$REMOTE_STATE_BUCKET" \
+  backend-config="key=$REMOTE_STATE_KEY" \
+  backend-config="encrypt=true" \
+  backend-config="region=$TF_VAR_region"
 
 # Try and pull the remote state if there is any
 terraform remote pull
 
 # run the command decrypting KMS variables
-shush --region $TF_VAR_region exec -- $@ 
+shush --region $TF_VAR_region exec -- $@
 
-# make sure the state is pushed
-terraform remote push
