f277c029417fcf0e4c4d52aeeaf56fbd2e17dc3e
kerry.wang <kerry.wang@myob.com>
2017-04-03 10:18:12 +1000

    update terraform from 0.8.8 to 0.9.2

--- terraform-shush/entrypoint.sh
+++ terraform-shush/entrypoint.sh
@@ -2,24 +2,13 @@
 
 set -eu -o pipefail
 
-cleanup() {
-  # make sure the state is pushed
-  terraform remote push
-}
-
-trap cleanup EXIT
-
 # Setup remote state in artifactory
-terraform remote config \
-  -backend=s3 \
+terraform init \
   -backend-config="bucket=$REMOTE_STATE_BUCKET" \
   -backend-config="key=$REMOTE_STATE_KEY" \
+  -backend-config="kms_key_id=$KMS_KEY" \
   -backend-config="encrypt=true" \
   -backend-config="region=$TF_VAR_region"
 
-# Try and pull the remote state if there is any
-terraform remote pull
-
 # run the command decrypting KMS variables
 shush --region $TF_VAR_region exec -- $@
-
