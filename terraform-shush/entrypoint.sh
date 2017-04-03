3091659a15396563f79fd65613b2014b57ee3f29
kerry.wang <kerry.wang@myob.com>
2017-04-03 13:27:36 +1000

    use default aws/s3 kms key

--- terraform-shush/entrypoint.sh
+++ terraform-shush/entrypoint.sh
@@ -6,7 +6,6 @@ set -eu -o pipefail
 terraform init \
   -backend-config="bucket=$REMOTE_STATE_BUCKET" \
   -backend-config="key=$REMOTE_STATE_KEY" \
-  -backend-config="kms_key_id=$KMS_KEY" \
   -backend-config="encrypt=true" \
   -backend-config="region=$TF_VAR_region"
 
